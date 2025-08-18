4ae989d5acbb5f367934a2a467d39009f3aa2dce
djahasiel101600 <djahasiel101600@gmail.com>
2025-08-18 18:34:06 +0800

    Adjust Barcode Scanner delay to .5sec

--- src/widgets/scanner-widget/BarcodeScannerWidget.tsx
+++ src/widgets/scanner-widget/BarcodeScannerWidget.tsx
@@ -8,7 +8,12 @@ const BarcodeScannerWidget = ({ onScan }: BarcodeScanner) => {
   return (
     <>
       <div className="relative rounded-2xl overflow-hidden z-0">
-        <BarcodeScanner onUpdate={onScan} width={500} height={500} />
+        <BarcodeScanner
+          delay={500}
+          onUpdate={onScan}
+          width={500}
+          height={500}
+        />
         <div className="overlay-box"></div>
       </div>
     </>
