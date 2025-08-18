e4fa3c72dd7cf7844e739548e3f7ed8ae8b94a16
djahasiel101600 <djahasiel101600@gmail.com>
2025-08-18 18:58:37 +0800

    Code fix 101

--- src/features/scanner/lib/handleScan.ts
+++ src/features/scanner/lib/handleScan.ts
@@ -1,6 +1,16 @@
+import type { Product } from "@/entities/product/model/product.types";
+import { getProducts } from "@/shared/api";
+
 function handleScan(err: any, result: any) {
+    
     if(!err && result){
         console.log(result.text);
+        let products: Product[] = []
+        getProducts().then(data => products = data).catch(error => console.log(error))
+        const matchedProduct = products.filter(prod => prod.barcode === result.text)
+        console.log("Barcode: ",matchedProduct[0].barcode)
+        console.log("Name: ",matchedProduct[0].name)
+        console.log("Price: ",matchedProduct[0].price)
     }
 }
 
