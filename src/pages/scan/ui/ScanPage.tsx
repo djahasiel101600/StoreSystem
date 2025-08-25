247fe5f306beb4703a320636788baf13ac9d1586
djahasiel101600 <djahasiel101600@gmail.com>
2025-08-25 13:50:11 +0800

    add link for login

--- src/pages/scan/ui/ScanPage.tsx
+++ src/pages/scan/ui/ScanPage.tsx
@@ -1,10 +1,13 @@
 import { ProductList } from "@/entities/product/ui/ProductList";
 import BarcodeScanner from "@/features/scanner/ui/scanner";
 import BarcodeSearchbar from "@/shared/ui/barcodeSearchbar";
+import { Link } from "react-router-dom";
+import { ROUTES } from "@/shared/config";
 
 const ScanPage = () => {
   return (
     <div className="p-4">
+      <Link to={ROUTES.LOGIN}>Login Here</Link>
       <h1 className="text-2xl p-2">Barcode Scanner</h1>
       <BarcodeScanner />
       <BarcodeSearchbar />
